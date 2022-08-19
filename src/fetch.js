f67aca1a239da2b1c3d4c2391f5247a4fb3239b9
whopper-wrapper <108198496+whopper-wrapper@users.noreply.github.com>
2022-08-20 05:31:30 +0900

    Update fetch.js

--- src/fetch.js
+++ src/fetch.js
@@ -7,12 +7,13 @@ async function onLoad() {
         var data = await res.text();
         //var config = JSON.parse(data.match(new RegExp(/<title>(.*)<\/title>/))[1]);
         //var config = data.match(new RegExp(/<title>(.*)<\/title>/))[1]; // OK
-        var config = data.match(new RegExp(/"config":{"csrf_token":"([^"]*)"/))[1];
+        //var config = data.match(new RegExp(/"config":{"csrf_token":"([^"]*)"/))[1]; //NG
+        var config = JSON.parse(data.match(new RegExp(/"resource":{"__dr":"PolarisProfileRoot.react"},"props":{(.*)}/))[1]);
         //var div1 = document.getElementById('csrf');
         //div1.insertAdjacentHTML('afterend', JSON.stringify({config}));
         //document.getElementById("csrf").innerHTML = JSON.stringify({config});
         //document.write(JSON.stringify({config}));
-        document.write(config);
+        document.write(JSON.stringify({config}));
     }
 };
 
