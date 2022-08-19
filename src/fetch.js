622638a5670a4d892c23e93e1a61681cc46039b1
whopper-wrapper <108198496+whopper-wrapper@users.noreply.github.com>
2022-08-20 00:25:14 +0900

    Update fetch.js

--- src/fetch.js
+++ src/fetch.js
@@ -2,8 +2,8 @@ async function onLoad() {
     var params = new URLSearchParams(window.location.search);
     if (params.has("username") == true) {
         var username = params.get("username");
-        var pubkey = `https://images${~~(Math.random() * 3333)}-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=https://www.instagram.com/${encodeURI(username)}/`;
-        var res = await fetch(pubkey);
+        var url = `https://images${~~(Math.random() * 3333)}-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=${encodeURI(`https://www.instagram.com/${username}/`)}`
+        var res = await fetch(url);
         var data = await res.text();
         var config = JSON.parse(data.match(new RegExp(/"config":{"csrf_token":"([^"]*)"/))[1]);
         //var div1 = document.getElementById('csrf');
